8ed5d2181046e3e878353eb132e6067d4f32c727
SirMalamute <neeliyer14@gmail.com>
2024-03-10 14:41:46 +0000

    removed subjectivity measure

--- Hello.py
+++ Hello.py
@@ -27,11 +27,11 @@ def run():
   prompt = st.chat_input("Say something")
   if prompt:
       st.write(f"Article Inputted: {prompt}")
-      st.write(f"Article Subjectivity (0.0-1.0):  {TextBlob(prompt).sentiment.subjectivity}")
+   #   st.write(f"Article Subjectivity (0.0-1.0):  {TextBlob(prompt).sentiment.subjectivity}")
       prompt2 = "rewrite the following text to be concise, fact driven, objective, neutral statements that aren't lengthy. they shouldnt hold any political affiliation but be fact driven and short. u should have a formal, neutral tone and not express any one view but present both sides equally and fairly: "
       response = model.generate_content(prompt2 + prompt)
       st.write(f"Rewritten Article: {response.text}")
-      st.write(f"Rewritten Article Subjectivity (0.0-1.0): {TextBlob(response.text).sentiment.subjectivity}" )
+    #  st.write(f"Rewritten Article Subjectivity (0.0-1.0): {TextBlob(response.text).sentiment.subjectivity}" )
 
 if __name__ == "__main__":
     run()
